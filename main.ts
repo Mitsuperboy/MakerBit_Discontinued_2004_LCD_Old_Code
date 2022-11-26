56e1aba948a3e55d3ce733a58f8fdd67cd567922
Mitsuperboy <47010140+Mitsuperboy@users.noreply.github.com>
2022-11-26 14:42:53 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+makerbit.connectLcd(39)
+makerbit.clearLcd2004()
+makerbit.setLcdBacklight(LcdBacklight.Off)
 basic.forever(function () {
-	
+    makerbit.showStringOnLcd2004("MakerBit", makerbit.position2004(LcdPosition2004.P0), 20)
+    if (makerbit.isLcdConnected()) {
+    	
+    }
 })
